24fe71a4b0e88eb6e86dad5d6464f488382d7a34
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-19 12:56:34 -0700

    calc_flags.c - edit: const int, for to return

--- calc_flags.c
+++ calc_flags.c
@@ -11,12 +11,12 @@ int calc_flags(const char *format, int *a)
 	int j, curr_a;
 	int flags = 0;
 	const char FL_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FL_ARR[] = {FN_SPACE, FN_MINUS, FN_ZERO, FN_PLUS, FN_HASH, 0};
+	const int FL_ARR[] = {FN_MINUS, FN_PLUS, FN_ZERO, FN_HASH, FN_SPACE, 0};
 
-	for (curr_a = *index + 1; format[curr_a] != '\0'; curr_a++)
+	for (curr_a = *a + 1; format[curr_a] != '\0'; curr_a++)
 	{
-		for (j = 0; FL_[j] != '\0'; j++)
-			if (format[current_i] == FL_CH[j])
+		for (j = 0; FL_CH[j] != '\0'; j++)
+			if (format[curr_a] == FL_CH[j])
 			{
 				flags |= FL_ARR[j];
 				break;
