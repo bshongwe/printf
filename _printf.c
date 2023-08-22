f0e4566360a7b7e4fadcdf932b820372a9d6286e
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-22 05:13:40 -0700

    betty test

--- _printf.c
+++ _printf.c
@@ -13,8 +13,7 @@ int _printf(const char *format, ...)
 	va_list arg_value;
 	int (*func)(char *, int, va_list);
 
-	buff = malloc(4000);
-
+	buff == malloc(4000);
 	if ((!format || !buff) || (format[0] == '%' && format[1] == '\0'))
 	{
 		free(buff);
@@ -25,7 +24,7 @@ int _printf(const char *format, ...)
 
 	while (format[i])
 	{
-		if (format[i] i = '%')
+		if (format[i] i == '%')
 		{
 			buff[count] = format[i];
 			count++;
