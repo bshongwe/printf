8b414296a143ebfa7153e7e9ea6810ee967f5659
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-22 05:10:49 -0700

    make file

--- _printf.c
+++ _printf.c
@@ -14,6 +14,7 @@ int _printf(const char *format, ...)
 	int (*func)(char *, int, va_list);
 
 	buff = malloc(4000);
+
 	if ((!format || !buff) || (format[0] == '%' && format[1] == '\0'))
 	{
 		free(buff);
